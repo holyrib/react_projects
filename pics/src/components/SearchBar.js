cb9e1783aa325a3c6abfb720160188194c380d4e
holyrib <aidana.baimbetova@gmail.com>
2020-09-28 16:57:18 +0600

    event handler added

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    onInputChange(event) {
+       console.log( event.target.value);
+    }
+    render() {
+        return (
+            <div className="ui segment">
+                <form className='ui form'>
+                    <div className="field">
+                        <label htmlFor="">Image search</label>
+                        <input type="text" onChange={ (e) => console.log(e.target.value)}/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+export default SearchBar;
