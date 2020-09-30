f5ac76425f1d0c97982fd7105496c9f032a78299
holyrib <aidana.baimbetova@gmail.com>
2020-09-30 16:17:42 +0600

    css added

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,12 +1,15 @@
+import './ImageList.css';
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
     const images = props.images.map((image) => {
-        return <img src={image.urls.regular} alt=""/>
+        return <ImageCard key={image.id} image={image}/>;
     });
 
+    return <div className="image-list">{images}</div>
 
-    return <div>{images}</div>;
+
 };
 
 export default ImageList;
