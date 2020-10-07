0d33b8e76ec54415b5c567c8854da2a45849b8b6
holyrib <aidana.baimbetova@gmail.com>
2020-10-07 15:39:45 +0600

    Dropdown with button added

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 const Dropdown = ( {options, selected, onSelectedChange} ) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+          document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
     const renderedOptions = options.map((option) => {
         if(option.value === selected.value){
             return null;
@@ -16,9 +31,8 @@ const Dropdown = ( {options, selected, onSelectedChange} ) => {
             </div>
         );
     });
-
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div
